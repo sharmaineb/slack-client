c90c75bdf20e2ca35cbc55d42af9d39064bf24ab
Sharmaine <sharmaine.borbe@students.dominican.edu>
2024-02-11 15:07:29 -0800

    almost done, i think.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useLazyQuery } from '@apollo/client';
+import PostsForChannel from './queries/PostsForChannel';
+import Channels from './queries/Channels';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [loadPosts, { called: postsCalled, loading: postsLoading, data: postsData }] = useLazyQuery(
+    PostsForChannel,
+    {
+      variables: { channel: 'Main' },
+    }
   );
+
+  const [loadChannels, { called: channelsCalled, loading: channelsLoading, data: channelsData }] = useLazyQuery(
+    Channels
+  );
+
+  if (postsCalled && postsLoading) {
+    return <div>Loading posts...</div>;
+  }
+
+  if (!postsCalled) {
+    return (
+      <div className="App">
+        <button onClick={() => loadPosts()}>Load Posts</button>
+      </div>
+    );
+  }
+
+  console.log(postsData);
+
+  if (postsData && postsData.posts) {
+    console.log(postsData); 
+    return (
+      <div className="App">
+        <h2>Posts:</h2>
+        {postsData.posts.map((post, index) => (
+          <p key={`${post.date}-${index}`}>{post.message} {post.date}</p>
+        ))}
+        <h2>Channels:</h2>
+        {channelsData && channelsData.channels ? (
+          <ul>
+            {channelsData.channels.map(channel => (
+              <li key={channel.id}>{channel.name}</li>
+            ))}
+          </ul>
+        ) : (
+          <div>No channels available</div>
+        )}
+        <button onClick={() => loadPosts()}>Load Posts</button>
+        <button onClick={() => loadChannels()}>Load Channels</button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { ApolloProvider, InMemoryCache, ApolloClient, split, HttpLink } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { WebSocketLink } from '@apollo/client/link/ws';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const httpLink = new HttpLink({
+  uri: 'http://localhost:5000/graphql', // Adjust the URI based on your GraphQL server endpoint
+});
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:5000/graphql', // Adjust the URI based on your GraphQL server endpoint
+  options: {
+    reconnect: true,
+  },
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === 'OperationDefinition' &&
+      definition.operation === 'subscription'
+    );
+  },
+  wsLink,
+  httpLink,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export const client = new ApolloClient({
+  link: splitLink,
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
